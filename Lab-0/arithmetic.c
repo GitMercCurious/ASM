8c0c6e612cc241bf3492bf3701228f3a1280c9c8
Merc Curious <motygin.vv@phystech.edu>
2023-09-07 02:27:35 +0300

    Creating simple C code and its ASM-listing

---
+++ Lab-0/arithmetic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int x, y, z;
+
+int main() {
+    x = 2;
+    y = 3;
+
+    z = x + y;
+
+    printf("%d\n", z);
+
+    return 0;
+}
---
+++ Lab-0/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+char word[] = "Hello";
+
+int main() {
+    printf("%s\n", word);
+
+    return 0;
+}
+
